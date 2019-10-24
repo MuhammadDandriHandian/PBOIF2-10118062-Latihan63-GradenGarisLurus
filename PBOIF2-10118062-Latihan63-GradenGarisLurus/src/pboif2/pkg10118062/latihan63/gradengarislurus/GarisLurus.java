9da8898612a9b65a5c4a035f4ec597de1f161ff3
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-10-24 17:43:36 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan63-GradenGarisLurus/src/pboif2/pkg10118062/latihan63/gradengarislurus/GarisLurus.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan63.gradengarislurus;
+
+/**
+ *
+ * @author 
+ * NAMA     : M Dandri Handian
+ * KELAS    : PBOIF2
+ * NIM      : 10118062
+ */
+public interface GarisLurus {
+    public int hitungGradient();
+}
